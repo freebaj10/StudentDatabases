6795981c1c6d326032ca5f7771cb769828c89b83
Jack Freebairn <144003627+freebaj10@users.noreply.github.com>
2024-05-20 09:31:10 +0100

    Initial commit

---
+++ CarbonTracker/Program.cs
@@ -0,0 +1,29 @@
+﻿using CarbonTracker.Data;
+using Microsoft.Extensions.Configuration;
+
+namespace CarbonTracker
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            const string ConnectionString = @"Data Source=C:\Natalie\GitRepo\C#\CarbonTracker\CarbonTracker.Data\Carbon.db;Mode=ReadWrite";
+
+			Console.WriteLine(ConnectionString);            
+            CarbonTrackerDb db = new(ConnectionString);
+
+            var names = db.GetUserNames();
+            Console.WriteLine("User Names:");
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Please enter a name to add:");
+            string newName = Console.ReadLine() ?? string.Empty;
+            db.AddUserName(newName);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Demo of calling SQL Lite from C#
+
+To get this demo to work you will need to change the connection string in Program.cs so that the data source is set to the path of Carbon.db on your machine<br/>
